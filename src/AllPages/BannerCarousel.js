ffb55879786dc94414f97b099b6fd1ef97b9e291
Trisha-tech <1234trishasahu@gmail.com>
2023-06-03 05:21:28 +0530

    Banner Carousel Updated

--- src/AllPages/BannerCarousel.js
+++ src/AllPages/BannerCarousel.js
@@ -1,34 +1,74 @@
-import Carousel from 'react-bootstrap/Carousel';
-import React, { useState } from 'react';
-import './BannerCarousel.css';
+import React from 'react';
+import Slider from "react-slick";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
 
-function BannerCarousel({ slides }) {
-  const [index, setIndex] = useState(0);
-
-  const handleSelect = (selectedIndex, e) => {
-    console.log('selected index: ', selectedIndex)
-    setIndex(selectedIndex);
-  };
+function BannerCarousel() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  }
 
   return (
-    <Carousel activeIndex={index}
-              onSelect={handleSelect}
-              nextIcon={<span aria-hidden="true" className="carousel-control-next-icon changed" />}
-    >
-      {slides.map((slide) => (
-        <Carousel.Item key={slide.image} interval={slide.interval}>
-          <img
-            className="d-block w-100"
-            src={slide.image}
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>{slide.title}</h3>
-            <p>{slide.subTitle}</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <Slider {...settings} style={{ margin: "2%" }}>
+      <div >
+        <Card sx={{ margin: "4%", backgroundImage: `linear-gradient(90deg, #A50383, #440336)`, borderRadius: "35px" }}>
+          <CardContent><br />
+            <Typography gutterBottom variant="h4" component="div" style={{ color: "white", paddingLeft: "50px", fontWeight: 700, fontSize: "57px" }}>
+              Win BIG with Fantasy Stocks
+            </Typography><br /><br />
+            <Box style={{ display: "flex" }}>
+              <Box style={{ width: "80%", paddingLeft: "50px" }}>
+                <Typography gutterBottom variant="h4" component="div" style={{ color: "white", display: "flex" }}>
+                  <img width="38" height="38" src="https://img.icons8.com/fluency/48/cheap-2.png" alt="cheap-2" />&nbsp;&nbsp; <span>Analyze and buy players</span>
+                </Typography><br />
+                <Typography gutterBottom variant="h4" component="div" style={{ color: "white", display: "flex" }}>
+                  <img width="38" height="38" src="https://img.icons8.com/fluency/48/cheap-2.png" alt="cheap-2" />&nbsp;&nbsp; <span> Build a portfolio of 4 players</span>
+                </Typography><br />
+                <Typography gutterBottom variant="h4" component="div" style={{ color: "white", display: "flex" }}>
+                  <img width="38" height="38" src="https://img.icons8.com/fluency/48/cheap-2.png" alt="cheap-2" /> &nbsp;&nbsp;<span>Sell high and cash in!</span>
+                </Typography><br />
+              </Box>
+              <Box style={{ float: "right" }}>
+                <img width="198" height="198" src="https://img.icons8.com/external-sbts2018-flat-sbts2018/58/000000/external-coin-bag-cryptocurrency-sbts2018-flat-sbts2018.png" alt="external-coin-bag-cryptocurrency-sbts2018-flat-sbts2018" />
+              </Box>
+            </Box>
+            <br />
+          </CardContent>
+        </Card>
+      </div>
+      <div >
+        <Card sx={{ margin: "4%", backgroundImage: `linear-gradient(90deg, #A50383, #440336)`, borderRadius: "35px" }}>
+          <CardContent><br />
+            <Typography gutterBottom variant="h4" component="div" style={{ color: "white", paddingLeft: "50px", fontWeight: 700, fontSize: "57px" }}>
+              Win BIG with Fantasy Stocks
+            </Typography><br /><br />
+            <Box style={{ display: "flex" }}>
+              <Box style={{ width: "80%", paddingLeft: "50px" }}>
+                <Typography gutterBottom variant="h4" component="div" style={{ color: "white", display: "flex" }}>
+                  <img width="38" height="38" src="https://img.icons8.com/fluency/48/cheap-2.png" alt="cheap-2" />&nbsp;&nbsp; <span>Analyze and buy players</span>
+                </Typography><br />
+                <Typography gutterBottom variant="h4" component="div" style={{ color: "white", display: "flex" }}>
+                  <img width="38" height="38" src="https://img.icons8.com/fluency/48/cheap-2.png" alt="cheap-2" />&nbsp;&nbsp; <span> Build a portfolio of 4 players</span>
+                </Typography><br />
+                <Typography gutterBottom variant="h4" component="div" style={{ color: "white", display: "flex" }}>
+                  <img width="38" height="38" src="https://img.icons8.com/fluency/48/cheap-2.png" alt="cheap-2" /> &nbsp;&nbsp;<span>Sell high and cash in!</span>
+                </Typography><br />
+              </Box>
+              <Box style={{ float: "right" }}>
+                <img width="178" height="178" src="https://img.icons8.com/external-sbts2018-flat-sbts2018/58/000000/external-coin-bag-cryptocurrency-sbts2018-flat-sbts2018.png" alt="external-coin-bag-cryptocurrency-sbts2018-flat-sbts2018" />
+              </Box>
+            </Box>
+            <br />
+          </CardContent>
+        </Card>
+      </div>
+    </Slider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,13 @@
 import React from 'react'
 import { Box } from '@mui/material';
-
 import SearchIcon from '@mui/icons-material/Search';
 import AccountCircleOutlined from '@mui/icons-material/AccountCircleOutlined'
 
-
-/*const useStyles = makeStyles((theme) => ({
-    component: {
-        display: "flex",
-        margin: "55px 130px 0 130px",
-        justifyContent: "space-between"
-    },
-    container: {
-        textAlign: "center",
-        padding: "12px 8px"
-    },
-    image: {
-        width: 64
-    },
-    text: {
-        fontSize: 14,
-        fontWeight: 600
-    }
-}))
-*/
 const Navbar = () => {
-    // const classes = useStyles();
     return (
-        <Box style={{textAlign:"right"}}>
-            <SearchIcon style={{fontSize:"55px",marginRight:"40px"}}/>
-            <AccountCircleOutlined style={{fontSize:"55px",marginRight:"40px"}} />
+        <Box style={{textAlign:"right",marginTop:"20px"}}>
+            <SearchIcon style={{fontSize:"55px",marginRight:"40px",color:"#A50383"}}/>
+            <AccountCircleOutlined style={{fontSize:"55px",marginRight:"40px",color:"#A50383"}} />
         </Box>
     )
 }
